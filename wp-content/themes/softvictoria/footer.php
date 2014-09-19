43f6bfec0c4353735e5c056bfb1d5f3d64ecc25b
Pedro Navarro <bytestriker@gmail.com>
2014-09-19 17:02:58 -0500

    subiendo tema personalizado

---
+++ wp-content/themes/softvictoria/footer.php
@@ -0,0 +1,8 @@
+			<div id='footer'>
+				<h1>This is the footer</h1>
+			</div>
+		</div>
+
+		<?php wp_footer(); ?>
+	</body>
+</html>
---
+++ wp-content/themes/softvictoria/header.php
@@ -0,0 +1,11 @@
+<html>
+	<head>
+		<title>My Theme</title>
+		<link rel='stylesheet' href='<?php bloginfo('stylesheet_url')?>'>
+	</head>
+	<body>
+		<div id='wrapper'>
+			<div id='header'>
+				<h1>This is the header</h1>
+			</div>
+		
