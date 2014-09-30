ff326c1687ddf126a47c3b815f9a60e37ba88c9d
JC Gaal <juangaal@gmail.com>
2014-09-29 22:36:06 -0500

    single page advanceç

--- wp-content/themes/softvictoria/sidebar.php
+++ wp-content/themes/softvictoria/sidebar.php
@@ -1,12 +1,19 @@
 		<div id='sidebar' class="large-3 columns">
-			<h2 class='sidebartitle'><?php _e('Categories');?></h2>
+			<div class='sidebartitle'>
+				<h2><?php _e('Categories');?></h2>
+			</div>
 			<ul>
+				<li>Mujer Awesome</li>
+				<li>Mujer poderosa</li>
+				<li>Mujer Fuerte</li>
+				<li>Mujer Hermosa</li>
 				<?php // wp_list_cats('sort_column=name&optioncount=1&hierarchical=0'); ?>
 			</ul>
 			
-			<div class='sidebartitle'><?php _e('Archives');?>
+			<div class='sidebartitle'>
+				<h2><?php _e('Te puede interesar');?></h2>
+			</div>
 				<ul>
 					<?php // wp_get_archives('type=monthly');?>
 				</ul>
-			</div>
 		</div>
--- wp-content/themes/softvictoria/single.php
+++ wp-content/themes/softvictoria/single.php
@@ -15,6 +15,9 @@ get_header(); ?>
 				<h1>The Post Title</h1>
 			</div>
 			<img src="http://placehold.it/720x350&text=imagen" class="custom-thumbnail-class">
+
+			<blockquote>Those people who think they know everything are a great annoyance to those of us who do.<cite>Isaac Asimov</cite></blockquote>
+			<p>
 			<?php 
 			
 			$post_id =  get_the_id();
@@ -22,7 +25,8 @@ get_header(); ?>
 			// $content = get_the_content();
 			print($objPost->post_content);
 			?>
-		</div><!-- #content -->
+			</p>
+		</div><!-- #the-post -->
 		<?php get_sidebar(); ?>
 	</div><!-- #primary -->
 
