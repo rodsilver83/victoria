d37d74c3b1f600718f8705701795ce7bdca50596
JC Gaal <juangaal@gmail.com>
2014-09-30 01:45:25 -0500

    single page ready and category page too

--- wp-content/themes/softvictoria/category.php
+++ wp-content/themes/softvictoria/category.php
@@ -5,51 +5,57 @@
 
 get_header(); ?> 
 
-<section id="primary" class="site-content">
-<div id="content" role="main">
+<div id="primary" class="row site-content">
+	<div id="content" role="main">
 
 <?php 
 // Check if there are any posts to display
 if ( have_posts() ) : ?>
 
-<header class="archive-header">
-<h1 class="archive-title">Category: <?php single_cat_title( '', false ); ?></h1>
+		<div role="main" id="the-category" class="large-9 columns">	
+			<div class="archive-header">
+				<div class="the-category-title">
+					<?php single_cat_title( '', false ); ?>
+					<?php
+					// Display optional category description
+					 if ( category_description() ) : 
+						?>
+					<?php echo category_description(); ?>
+					<?php endif; ?>
+				</div>
+			</div>
+
+			<?php
+
+			// The Loop
+			while ( have_posts() ) : the_post();
+			?>
+			<div class="category-post">
+				<div class="category-post-title">
+					<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+					<small><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></small>
+				</div>
+
+				<div class="the-category-entry">
+					<?php the_content(); ?>
+
+				 <!--p class="postmetadata">
+				 	<?//php comments_popup_link( 'No comments yet', '1 comment', '% comments', 'comments-link', 'Comments closed'); ?>
+				 </p-->
+				</div>
+			</div>
+
+			<?php endwhile; 
+
+				else: ?>
+
+			<p>Sorry, no posts matched your criteria.</p>
 
 
-<?php
-// Display optional category description
- if ( category_description() ) : 
-	?>
-<div class="archive-meta"><?php echo category_description(); ?></div>
-<?php endif; ?>
-</header>
-
-<?php
-
-// The Loop
-while ( have_posts() ) : the_post();
-?>
-<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-<small><?php the_time('F jS, Y') ?> by <?php the_author_posts_link() ?></small>
-
-<div class="entry">
-<?php the_content(); ?>
-
- <p class="postmetadata"><?php
-  comments_popup_link( 'No comments yet', '1 comment', '% comments', 'comments-link', 'Comments closed');
-?></p>
+		<?php endif; ?>
+		</div>
+		<?php get_sidebar(); ?>
+	</div>
 </div>
 
-<?php endwhile; 
-
-else: ?>
-<p>Sorry, no posts matched your criteria.</p>
-
-
-<?php endif; ?>
-</div>
-</section>
-
-
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
--- wp-content/themes/softvictoria/sidebar.php
+++ wp-content/themes/softvictoria/sidebar.php
@@ -2,18 +2,54 @@
 			<div class='sidebartitle'>
 				<h2><?php _e('Categories');?></h2>
 			</div>
-			<ul>
-				<li>Mujer Awesome</li>
-				<li>Mujer poderosa</li>
-				<li>Mujer Fuerte</li>
-				<li>Mujer Hermosa</li>
+			<ul class="no-bullet cat-list">
+				<li>
+					<a href="">
+						<i class="general foundicon-right-arrow"> Mujer Awesome</i>
+					</a>
+				</li>
+				<li>
+					<a href="">	
+						<i class="general foundicon-right-arrow"> Mujer poderosa</i>
+					</a>
+				</li>
+				<li>
+					<a href="">	
+						<i class="general foundicon-right-arrow"> Mujer Fuerte</i>
+					</a>
+				</li>
+				<li>
+					<a href="">	
+						<i class="general foundicon-right-arrow"> Mujer Hermosa</i>
+					</a>
+				</li>
 				<?php // wp_list_cats('sort_column=name&optioncount=1&hierarchical=0'); ?>
 			</ul>
 			
 			<div class='sidebartitle'>
 				<h2><?php _e('Te puede interesar');?></h2>
 			</div>
-				<ul>
+				<ul class="no-bullet cat-list">
+					<li>
+						<a href="">
+							<i class="general foundicon-right-arrow"> Mujer Awesome</i>
+						</a>
+					</li>
+					<li>
+						<a href="">	
+							<i class="general foundicon-right-arrow"> Mujer poderosa</i>
+						</a>
+					</li>
+					<li>
+						<a href="">	
+							<i class="general foundicon-right-arrow"> Mujer Fuerte</i>
+						</a>
+					</li>
+					<li>
+						<a href="">	
+							<i class="general foundicon-right-arrow"> Mujer Hermosa</i>
+						</a>
+					</li>
 					<?php // wp_get_archives('type=monthly');?>
 				</ul>
 		</div>
